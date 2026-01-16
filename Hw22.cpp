3ef54723fba1d87ae828a2b039868222ade90966
NickM11 <43708706+NickM11@users.noreply.github.com>
2026-01-16 13:54:21 +0100

    Hw22 test

---
+++ Hw22.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <string>
+
+class Character
+{
+
+class Weapon
+{
+	struct Stats
+	{
+		int damage;
+		int cooldown;
+	};
+
+	struct Icon
+	{
+		int size;
+		std::string type;
+	};
+};
+
+class PassiveItems
+{
+
+
+struct modifiedAttributes
+{
+	int damage;
+	int cooldown;
+};
+
+struct Icon
+{
+	int size;
+	std::string type;
+};
+public:
+	modifiedAttributes modfA;
+	
+	void ChangeAtributes(modifiedAttributes modfA) {
+	}
+
+};
+public:
+	int health;
+
+	Weapon weapon;
+
+	PassiveItems passiveitems;
+
+	struct Atributes
+	{
+		int strength;
+		int dexterity;
+		int agility;
+	};
+private:
+	
+
+
+};
+
+
+int main() {
+	Character character;
+	character.health = 100;
+	character.passiveitems.modfA
+}
